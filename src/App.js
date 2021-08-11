5cd9e63233c6c9bc9805780832954ff1a283364c
errea <erreakezy@gmail.com>
2021-08-11 10:16:35 +0100

    Modify App.js + add constructor to class based component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Calculator from './components/calculator';
 import './componentsStyles/calculatorStyles.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <div className="App">
