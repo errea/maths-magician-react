75889da561f30e7295fcaf45ae3e802109d46360
errea <erreakezy@gmail.com>
2021-08-12 16:33:45 +0100

    Modify buttons.js + refactor code to use Hooks

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,64 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleStateChange = this.handleStateChange.bind(this);
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-  }
+const Button = () => {
+  const [total, setTotal] = useState('');
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
+  const obj = {
+    total,
+    next,
+    operation,
+  };
 
-  handleStateChange(newState) {
+  const handleStateChange = (newState) => {
     if (newState.total != null) {
-      this.setState({ total: newState.total });
+      setTotal(newState.total);
     }
 
-    this.setState({ next: newState.next });
+    setNext(newState.next);
 
     if (newState.operation != null) {
-      this.setState({ operation: newState.operation });
+      setOperation(newState.operation);
     }
-  }
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator-container">
-        <div className="resultShow-container">
-          <p className="resultNumber-display">
-            { total }
-            { ' ' }
-            { operation }
-            { ' ' }
-            { next }
-          </p>
-        </div>
-        <button className="number0 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '0')); }}>0</button>
-        <button className="number1 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '1')); }}>1</button>
-        <button className="number2 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '2')); }}>2</button>
-        <button className="number3 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '3')); }}>3</button>
-        <button className="number4 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '4')); }}>4</button>
-        <button className="number5 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '5')); }}>5</button>
-        <button className="number6 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '6')); }}>6</button>
-        <button className="number7 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '7')); }}>7</button>
-        <button className="number8 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '8')); }}>8</button>
-        <button className="number9 numbers" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '9')); }}>9</button>
-        <button className="buttonAc symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, 'AC')); }}>AC</button>
-        <button className="plusminus symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '+/-')); }}>+/-</button>
-        <button className="percentage symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '%')); }}>%</button>
-        <button className="plus symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '+')); }}>+</button>
-        <button className="minus symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '-')); }}>-</button>
-        <button className="divide symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '÷')); }}>÷</button>
-        <button className="multiply symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, 'x')); }}>x</button>
-        <button className="dot symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '.')); }}>.</button>
-        <button className="equal symbols" type="button" onClick={() => { this.handleStateChange(calculate(this.state, '=')); }}>=</button>
+  return (
+    <div className="calculator-container">
+      <div className="resultShow-container">
+        <p className="resultNumber-display">
+          { total }
+          { ' ' }
+          { operation }
+          { ' ' }
+          { next }
+        </p>
       </div>
-    );
-  }
-}
+      <button className="number0 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '0')); }}>0</button>
+      <button className="number1 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '1')); }}>1</button>
+      <button className="number2 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '2')); }}>2</button>
+      <button className="number3 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '3')); }}>3</button>
+      <button className="number4 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '4')); }}>4</button>
+      <button className="number5 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '5')); }}>5</button>
+      <button className="number6 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '6')); }}>6</button>
+      <button className="number7 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '7')); }}>7</button>
+      <button className="number8 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '8')); }}>8</button>
+      <button className="number9 numbers" type="button" onClick={() => { handleStateChange(calculate(obj, '9')); }}>9</button>
+      <button className="buttonAc symbols" type="button" onClick={() => { handleStateChange(calculate(obj, 'AC')); }}>AC</button>
+      <button className="plusminus symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '+/-')); }}>+/-</button>
+      <button className="percentage symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '%')); }}>%</button>
+      <button className="plus symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '+')); }}>+</button>
+      <button className="minus symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '-')); }}>-</button>
+      <button className="divide symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '÷')); }}>÷</button>
+      <button className="multiply symbols" type="button" onClick={() => { handleStateChange(calculate(obj, 'x')); }}>x</button>
+      <button className="dot symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '.')); }}>.</button>
+      <button className="equal symbols" type="button" onClick={() => { handleStateChange(calculate(obj, '=')); }}>=</button>
+    </div>
+  );
+};
 
 export default Button;
