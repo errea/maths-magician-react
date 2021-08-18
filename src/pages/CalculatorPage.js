629f007a26cbfe32047406a00e19c2a70fdb7d84
errea <erreakezy@gmail.com>
2021-08-18 10:51:36 +0100

    Review-fix error imported file name isn't matching the existing one in CalculatorPage.js

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import Calculator from '../components/Calculator.js';
+import Calculator from '../components/calculator.js';
 import style from './CalculatorPage.module.css';
 
 const CalculatorPage = () => (
